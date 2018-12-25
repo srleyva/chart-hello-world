7ba8e38e90c825b0d010d96a2e87f4e1423675bf
Stephen Leyva <sleyva@staq.com>
2018-12-25 16:23:12 -0500

    new app for the hello-world

--- main.go
+++ main.go
@@ -2,15 +2,79 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"strconv"
 )
 
-func main() {
-	http.HandleFunc("/", sayHello)
-	log.Fatal(http.ListenAndServe(":80", nil))
+// Resource in the REST API
+type Resource interface {
+	Get(c echo.Context) error
+	Put(c echo.Context) error
+	Post(c echo.Context) error
+	Delete(c echo.Context) error
 }
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from %s", r.URL.Path)
+// Vending machine
+type Vending struct {
+	coins     int
+	chocolate int
+	cost      int
+}
+
+// Get the information about the vending machine
+func (v *Vending) Get(c echo.Context) error {
+	if v.chocolate == 0 {
+		return c.String(http.StatusOK, "Out of chocolate please refill")
+	}
+	return c.String(http.StatusOK, fmt.Sprintf("There are %v pieces of chocolate left.", v.chocolate))
+}
+
+// Post will refill the vending machine with the chocolate
+func (v *Vending) Post(c echo.Context) error {
+	refill, err := strconv.Atoi(c.Param("amount"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Please fill with chocolate")
+	}
+	v.chocolate += refill
+	return c.String(http.StatusOK, "Vending machine refilled")
+}
+
+// Put will return the chocolate in exchange for the coins
+func (v *Vending) Put(c echo.Context) error {
+	coins, err := strconv.Atoi(c.Param("amount"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Thats not a coin! No chocolate for you!")
+	}
+	customerAmount := coins / v.cost
+	if customerAmount > v.chocolate {
+		return echo.NewHTTPError(http.StatusBadRequest, "There is not enough chocolate to give you")
+	}
+	v.chocolate -= customerAmount
+	v.coins += coins
+	return c.String(http.StatusOK, fmt.Sprintf("You now have %v pieces of chocolate", customerAmount))
+}
+
+func main() {
+	e := echo.New()
+	machine := Vending{
+		coins:     0,
+		chocolate: 10,
+		cost:      1,
+	}
+
+	e.Logger.SetLevel(log.INFO)
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+
+	e.GET("/vending", machine.Get)
+	e.POST("/vending/:amount", machine.Post)
+	e.PUT("/vending/:amount", machine.Put)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
