2e0a6da841568eab18b8d5c0c1171ec504ba1741
Stephen Leyva <sleyva@staq.com>
2018-12-26 15:04:08 -0500

    add RED metrics for http requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/0neSe7en/echo-prometheus"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
@@ -69,6 +70,7 @@ func main() {
 	e.Logger.SetLevel(log.INFO)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(echoprometheus.NewMetricWithConfig(echoprometheus.PrometheusConfig{Namespace: "vending"}))
 
 	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
